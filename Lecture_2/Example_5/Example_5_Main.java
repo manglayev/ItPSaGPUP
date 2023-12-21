28ccf249b2ccf6efdbd7b42c300b101e019dacc8
Talgat Manglayev <talgatmanglayev@MacBook-Pro-Talgat.local>
2023-12-21 11:41:02 +0600

    added examples with runnable and thread sleep time

--- Lecture_2/Example_5/Example_5_Main.java
+++ Lecture_2/Example_5/Example_5_Main.java
@@ -2,12 +2,11 @@ public class Example_5_Main
 {
     public static void main(String[] args)
     {
-      int size = 5;
-      Thread[] threads = new Thread[size];
-      for (int a = 0; a < size; a++)
-      {
-          threads[a] = new Thread( new Example_5(a) );
-          threads[a].start();
-      }
+      Example_5_1 example1 = new Example_5_1();
+      Example_5_2 example2 = new Example_5_2(123);
+      Thread thread1 = new Thread(example1);
+      Thread thread2 = new Thread(example2);
+      thread1.start();
+      thread2.start();
     }
 }
