98bdc609e018c8c9962d800a9ee93c9c01392094
Talgat-qypshaq <talgat.manglayev@gmail.com>
2024-01-23 11:52:26 +0600

    added deadlock examples

--- Lecture_3/Example_15/Example_15_Main.java
+++ Lecture_3/Example_15/Example_15_Main.java
@@ -5,7 +5,6 @@ class Example_15_Main
 	public static void main(String[] args) throws InterruptedException
 	{
 		System.out.println("Main thread execution starts");
-    // Thread priorities are set in a way that thread[size - 1] gets least priority.
     int size = 4;
     Thread[] threads = new Thread[size];
     for (int a = 0; a < size; a++)
@@ -15,10 +14,6 @@ class Example_15_Main
     for (int a = 0; a < size; a++)
     {
         threads[a].start();
-    }
-    for (int a = 0; a < size; a++)
-    {
-        threads[a].join();
     }
 		System.out.println("Main thread execution completes");
 	}
